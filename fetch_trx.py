ea5e7678a09cb0d9b75f39f17b7db49a23ff9ada
juejiang1998 <944141309@qq.com>
2026-04-17 18:22:03 +0800

    Create fetch_trx.py

---
+++ fetch_trx.py
@@ -0,0 +1,31 @@
+import os
+import json
+import requests
+
+api_key = os.getenv("CMC_API_KEY")
+
+url = "https://pro-api.coinmarketcap.com/v3/cryptocurrency/quotes/latest"
+
+headers = {
+    "X-CMC_PRO_API_KEY": api_key
+}
+
+params = {
+    "symbol": "TRX",
+    "convert": "USD"
+}
+
+res = requests.get(url, headers=headers, params=params)
+data = res.json()
+
+trx = data["data"]["TRX"][0]["quote"]["USD"]
+
+output = {
+    "price": trx["price"],
+    "percent_change_24h": trx["percent_change_24h"],
+    "market_cap": trx["market_cap"],
+    "last_updated": trx["last_updated"]
+}
+
+with open("data.json", "w") as f:
+    json.dump(output, f)
