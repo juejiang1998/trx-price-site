7cf8d64e685bbd12af3116334487477082bdba6a
juejiang1998 <944141309@qq.com>
2026-04-17 18:36:28 +0800

    Update fetch_trx.py

--- fetch_trx.py
+++ fetch_trx.py
@@ -24,27 +24,8 @@ res.raise_for_status()
 
 data = res.json()
 
-# v3 文档里 data 是数组，先取第一个资产
-trx_info = data["data"][0]
+print("完整返回：")
+print(json.dumps(data, ensure_ascii=False, indent=2))
 
-# 有些返回里 quote 可能直接是对象，也可能被包成列表，做兼容处理
-quote = trx_info["quote"]
-if isinstance(quote, list):
-    quote = quote[0]
-
-usd = quote["USD"]
-if isinstance(usd, list):
-    usd = usd[0]
-
-output = {
-    "price": usd["price"],
-    "percent_change_24h": usd["percent_change_24h"],
-    "market_cap": usd["market_cap"],
-    "volume_24h": usd["volume_24h"],
-    "last_updated": usd["last_updated"]
-}
-
-with open("data.json", "w", encoding="utf-8") as f:
-    json.dump(output, f, ensure_ascii=False, indent=2)
-
-print("TRX 数据更新成功")
+# 先故意停在这里，先看真实结构
+raise Exception("先看上面打印出来的数据结构")
